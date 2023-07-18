7e46033f942b638b4056aaf6e163c548aa26803c
br <236543611@qq.com>
2023-07-18 15:05:42 +0800

    2023年7月18日15:05:41

---
+++ components/FormFieldText.vue
@@ -0,0 +1,15 @@
+<template>
+    <div :class="meta" class="flex flex-col">
+        <label :for="name" class="font-bold my-2">{{ name }}</label>
+        <input :type="type" v-model="value" :name="name" :id="name" class="border rounded p-2">
+        <p v-if="errorMessage" class=" text-red-400">{{ errorMessage }}</p>
+    </div>
+</template>
+<script lang="ts" setup>
+import { useField } from 'vee-validate'
+const props = defineProps<{ name: string, label: string, type: string }>()
+
+const { value, meta, errorMessage } = useField(props.name)
+</script>
+
+<style scoped></style>
--- pages/form.vue
+++ pages/form.vue
@@ -1,7 +1,39 @@
 <template>
-    <div>form</div>
+    <div class="flex justify-center items-center h-full">
+        <div class="w-400px h-400px p-4 bg-blue-400 rounded ">
+            <form @submit.prevent="onSubmit">
+                <FormFieldText name="username" type="email" label="Username"></FormFieldText>
+                <FormFieldText name="password" type="password" label="Password"></FormFieldText>
+                <n-button class="mt-4" type="primary" block :disabled="!meta.valid">Submit</n-button>
+            </form>
+        </div>
+
+    </div>
 </template>
 <script lang="ts" setup>
+
+import { NButton } from 'naive-ui';
+
+import * as yup from 'yup'
+
+
+const schema = yup.object({
+    username: yup.string().required("Username is required"),
+    password: yup.string().required("Password is required").min(6, "Password must be at least 6 characters").max(20, "Password must be less than 20 characters"),
+})
+
+interface UserCreate {
+    username: string
+    password: string
+}
+
+const { handleReset, handleSubmit, meta, values } = useForm<UserCreate>({
+    validationSchema: schema
+})
+const onSubmit = handleSubmit((values) => {
+    console.log(values)
+    handleReset()
+})
 </script>
 
 <style scoped></style>
